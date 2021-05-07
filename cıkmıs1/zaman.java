b3453dd3dd3fcd30545a84d1da161c3bd770fed7
Yağmur Gazezoğlu <33622890+ygazez@users.noreply.github.com>
2021-05-08 01:10:52 +0300

    Create zaman.java

---
+++ cıkmıs1/zaman.java
@@ -0,0 +1,27 @@
+public class Zaman {
+    private int yÄ±l;
+    private int ay;
+
+    public Zaman(int ay,int yÄ±l) {
+        this.ay = ay;
+        this.yÄ±l=yÄ±l;
+    }
+    public Zaman (){
+    }
+
+    public int getAy() {
+        return ay;
+    }
+
+    public void setAy(int ay) {
+        this.ay = ay;
+    }
+
+    public int getYÄ±l() {
+        return yÄ±l;
+    }
+
+    public void setYÄ±l(int yÄ±l) {
+        this.yÄ±l = yÄ±l;
+    }
+}
