9e93acef9e4f16ebceb0d30369509d66b9dfec26
Yağmur Gazezoğlu <33622890+ygazez@users.noreply.github.com>
2021-05-08 00:50:36 +0300

    Create aktor_test.java

---
+++ constractor/aktor_test.java
@@ -0,0 +1,22 @@
+
+public class aktör_test {
+
+	public static void main(String[] args) {
+		int dizi1[]= {};
+		int dizi2[]= {};
+		
+		
+		aktör a1=new aktör("leonado","dicaprio",1000000,350);
+		
+		System.out.println("A1 BİLGİLER:\n");
+		System.out.println(a1.getAd()+" "+a1.getSoyad()+" "+a1.getMaas()+" "+a1.getFilm_sayisi());
+		aktör.bilgi_gir();
+		
+		
+	
+		
+		
+
+	}
+
+}
